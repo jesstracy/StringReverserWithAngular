cc1640ce3e293549de03712e6c07b5fcd9b686c7
Jessica Tracy <j.tracy916@gmail.com>
2016-09-20 15:22:04 -0400

    Initial commit

---
+++ src/main/java/tiy/com/StringReverserAngularApplication.java
@@ -0,0 +1,12 @@
+package tiy.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringReverserAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringReverserAngularApplication.class, args);
+	}
+}
